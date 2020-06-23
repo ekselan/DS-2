400bc07586203948ad38ae954601d4bd2aa8e394
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-06-23 17:59:52 -0400

    First attempt loading pickled model

---
+++ model_test/recommend.py
@@ -0,0 +1,23 @@
+import pickle
+import pandas as pd
+
+# Pickled model filepath
+MODEL_FILEPATH = "/Users/ekselan/Desktop/Med_Cab_BW/DS-2/data/medcab_model.pkl"
+
+# Filepath for .csv dataset
+DATA_FILEPATH = "/Users/ekselan/Desktop/Med_Cab_BW/DS-2/data/BW_MedCab_Dataset_With_Index.csv"
+
+df = pd.read_csv(DATA_FILEPATH)
+print(df.shape)
+print(df.head())
+
+# def load_model():
+#     """Function to load pickled nearest neighbors model"""
+#     # print("LOADING THE MODEL...")
+#     with open(MODEL_FILEPATH, "rb") as model_file:
+#         saved_model = pickle.load(model_file)
+#     return saved_model
+
+# if __name__ == "__main__":
+
+#     recommender = load_model()
