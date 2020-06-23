aa8ba3b6b38f400c1074a000e649aa13b94ae98c
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-06-23 19:08:26 -0400

    Progress on flask deployment of pickled model

--- MedCab/__init__.py
+++ MedCab/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from flask_cors import CORS
 
 from MedCab.alt_routes.home_routes import home_routes
+from MedCab.alt_routes.model_routes import model_routes
 
 
 def create_app():
     app = Flask(__name__)
     cors = CORS(app)
     app.register_blueprint(home_routes)
+    app.register_blueprint(model_routes)
     return app
 
 
