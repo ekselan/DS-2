91e5609c0f9192171bcd825338306b86b247e7f3
Aaron Watkins <61791690+ekselan@users.noreply.github.com>
2020-06-23 20:25:54 -0400

    Tweak for heroku

--- MedCab/__init__.py
+++ MedCab/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 
-from MedCab.alt_routes.home_routes import home_routes, connection, cursor
+from MedCab.alt_routes.home_routes import home_routes, # connection, cursor
 # from MedCab.alt_routes.model_routes import model_routes
 
 def create_app():
