ab4dffd9aa1839b62c22bb2919e8a24a55a56973
britneh <brhacket@gmail.com>
2020-06-23 18:04:34 -0700

    tweaks

--- MedCab/__init__.py
+++ MedCab/__init__.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 
 from MedCab.alt_routes.home_routes import home_routes
 
-from MedCab.alt_routes.home_routes import home_routes, connection, cursor
+
 # from MedCab.alt_routes.model_routes import model_routes
 
 def create_app():
